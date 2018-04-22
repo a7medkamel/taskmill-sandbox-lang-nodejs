2ca94a0ab81a03297875a7a29571429f59327bdf
Ahmed Kamel <ak@ahmedkamel.net>
2018-04-22 00:16:56 -0700

    initial checkin

---
+++ bin/index.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+process.env.SUPPRESS_NO_CONFIG_WARNING = 'y';
+require('../').main();
---
+++ index.js
@@ -0,0 +1,55 @@
+var Promise       = require('bluebird')
+  // , config        = require('config')
+  , HTTP          = require('./lib/http')
+  , Repository    = require('./lib/repo')
+  , Code          = require('./lib/code')
+  ;
+
+Promise.config({
+  longStackTraces: true
+})
+
+process.on('unhandledRejection', (err, p) => {
+  console.error(new Date().toUTCString(), 'unhandledRejection', err.message);
+  console.error(err.stack);
+
+  // process.exit(1);
+});
+
+process.on('uncaughtException', (err) => {
+  console.error(new Date().toUTCString(), 'uncaughtException', err.message);
+  console.error(err.stack);
+
+  // process.exit(1);
+});
+
+function main() {
+  // let { secret, tailf, remote, sha, base_url, cmd, args } = config.get('sandbox');
+
+  Promise
+    .all([
+        (new HTTP()).listen({ port : 1337 })
+      , Repository.create()
+    ])
+    .spread((app, repo) => {
+      app.use((req, res, next) => {
+        (new Code({ repo }))
+          .run(req, res, next)
+          .catch((err) => {
+            res.status(500).send(err.toString());
+          });
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+}
+
+if (require.main === module) {
+  main();
+}
+
+module.exports = {
+  main
+};
